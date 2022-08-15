7641bec9900359703e1d966ab2a2fbb0ba3f43e0
Michael Larbi <mickylarbi28@gmail.com>
2022-08-15 19:16:28 +0000

    initial commit

---
+++ lib/src.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Src extends StatelessWidget {
+  const Src({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material();
+  }
+}
